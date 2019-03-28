bff48895188cd6ebc8b71821334743afca987608
SergeyK-WebDeveloper <Sergey.Kist@gmail.com>
2019-03-28 20:24:10 +0000

    ORM__RedBeenPHP 02_Подключение ORM

--- 00_RedBeenPHP__ORM/index.php
+++ 00_RedBeenPHP__ORM/index.php
@@ -8,4 +8,12 @@ require_once __DIR__ . '/vendor/autoload.php';
 //\RedBeanPHP\R::setup();
 //use \RedBeanPHP\R as R;
 class_alias('\RedBeanPHP\R', '\R');
-R::setup();
+
+R::setup( 'mysql:host=localhost;dbname=test', 'root', '' );
+//R::setup( 'sqlite:dbfile.db' );
+
+if( !R::testConnection() ){
+    die('No DB Connection');
+}
+
+echo 'OK';
